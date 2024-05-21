ffce18a45d059f970ec92d5fbcd837e6312e70ef
Samitama <90217942+Samitama@users.noreply.github.com>
2024-05-21 18:21:27 +0300

    main
    update

--- src/Examples_of_UART/katot.py
+++ src/Examples_of_UART/katot.py
@@ -1,14 +1,24 @@
 import serial
 import time
 
-port = serial.Serial('/dev/cu.usbmodem101',115200,timeout=1)
+port = serial.Serial('/dev/cu.usbserial-10',115200,timeout=1)
 time.sleep(3)
 
 while port.is_open:
-    port.write(b'#|1010|'+str(255).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
+    port.write(b'#|10|10|'+str(125).encode("ascii")+b'|'+str(125).encode("ascii")+b'|')
     time.sleep(5)
-    port.write(b'#|110|'+str(0).encode("ascii")+b'|'+str(255).encode("ascii")+b'|')
+    port.write(b'#|11|11|'+str(0).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
     time.sleep(5)
-    port.write(b'#|0000|'+str(0).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
+    port.write(b'#|01|01|'+str(125).encode("ascii")+b'|'+str(125).encode("ascii")+b'|')
+    time.sleep(5)
+    port.write(b'#|00|00|'+str(0).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
+    time.sleep(5)
+    port.write(b'#|10|01|'+str(125).encode("ascii")+b'|'+str(125).encode("ascii")+b'|')
+    time.sleep(5)
+    port.write(b'#|00|00|'+str(0).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
+    time.sleep(5)
+    port.write(b'#|01|10|'+str(125).encode("ascii")+b'|'+str(125).encode("ascii")+b'|')
+    time.sleep(5)
+    port.write(b'#|00|00|'+str(0).encode("ascii")+b'|'+str(0).encode("ascii")+b'|')
     time.sleep(5)
     
--- src/Examples_of_UART/sketch_jan4a.ino
+++ src/Examples_of_UART/sketch_jan4a.ino
@@ -1,13 +1,24 @@
-int data_length=3;
+int data_length=4;
 int data_array[64];
-int pins[6] = {3,4,5,6,10,11};
+// Motor 1 için hareket pinlerinin tanımı
+#define PWM1 3
+#define IN1 4 
+#define IN2 5
+
+// Motor 2 için hareket pinlerinin tanımı
+#define PWM2 6
+#define IN3 7
+#define IN4 8
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  for (int i=0; i<6; i++) {
-  pinMode(pins[i], OUTPUT);
-  }
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
 void loop() {
@@ -17,44 +28,44 @@ void loop() {
       for (int j=0; j<data_length; j++) {
       data_array[j] = Serial.parseInt();
       }
-      if(data_array[0] == 1010){
-        digitalWrite(pins[0], 1);
-        digitalWrite(pins[1], 0);
-        digitalWrite(pins[2], 1);
-        digitalWrite(pins[3], 0);
+      if(data_array[0] == 10 && data_array[1] == 10){
+          digitalWrite(IN1, HIGH);
+          digitalWrite(IN2, LOW);
+          digitalWrite(IN4, HIGH);
+          digitalWrite(IN3, LOW);
       }
-      if (data_array[0]== 1111) {
-        digitalWrite(pins[0], 1);
-        digitalWrite(pins[1], 1);
-        digitalWrite(pins[2], 1);
-        digitalWrite(pins[3], 1);
+      if (data_array[0]== 11 && data_array[1] ==11){
+          digitalWrite(IN1, HIGH);
+          digitalWrite(IN2, HIGH);
+          digitalWrite(IN4, HIGH);
+          digitalWrite(IN3, HIGH);
       }
-      if (data_array[0] == 0000) {
-        digitalWrite(pins[0], 0);
-        digitalWrite(pins[1], 0);
-        digitalWrite(pins[2], 0);
-        digitalWrite(pins[3], 0);
+      if (data_array[0] == 00 && data_array[1] == 00){
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, LOW);
+          digitalWrite(IN4, LOW);
+          digitalWrite(IN3, LOW);
       }
-      if (data_array[0]== 101) {
-        digitalWrite(pins[0], 0);
-        digitalWrite(pins[1], 1);
-        digitalWrite(pins[2], 0);
-        digitalWrite(pins[3], 1);
+      if (data_array[0] == 01 && data_array[1] == 01){
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, HIGH);
+          digitalWrite(IN4, LOW);
+          digitalWrite(IN3, HIGH);
       }
-      if (data_array[0]== 110) {
-        digitalWrite(pins[0], 0);
-        digitalWrite(pins[1], 1);
-        digitalWrite(pins[2], 1);
-        digitalWrite(pins[3], 0);
+      if (data_array[0] == 01 && data_array[1] == 10){
+          digitalWrite(IN1, LOW);
+          digitalWrite(IN2, HIGH);
+          digitalWrite(IN4, HIGH);
+          digitalWrite(IN3, LOW);
       }
-      if (data_array[0]== 1001) {
-        digitalWrite(pins[0], 1);
-        digitalWrite(pins[1], 0);
-        digitalWrite(pins[2], 0);
-        digitalWrite(pins[3], 1);
+      if (data_array[0]== 10 && data_array[1] == 01){
+          digitalWrite(IN1, HIGH);
+          digitalWrite(IN2, LOW);
+          digitalWrite(IN4, LOW);
+          digitalWrite(IN3, HIGH);
       }
-      analogWrite(pins[4], data_array[1]);
-      analogWrite(pins[5], data_array[2]);
+      analogWrite(PWM1, data_array[2]);
+      analogWrite(PWM2, data_array[3]);
     }
   }  
 }
